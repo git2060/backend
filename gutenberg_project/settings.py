48b5048620e68104b9f0c0385b77f9908169cd10
git2060 <shyamgundetin@gmail.com>
2025-02-22 05:52:29 +0530

    Update settings.py

--- gutenberg_project/settings.py
+++ gutenberg_project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'gutenberg_books',
         'USER':'root',
         'PASSWORD':'admin',
-        'HOST':'0.0.0.0',
+        'HOST':'35.160.120.126',
         'PORT':3306
     }
 }
